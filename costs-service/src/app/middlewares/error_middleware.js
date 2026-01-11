72db7ce7cdc0750c7a5deaddcc14e7ee1918adfe
Elad Hayek <elad3283@gmail.com>
2026-01-11 22:59:37 +0200

    handle error ids

--- costs-service/src/app/middlewares/error_middleware.js
+++ costs-service/src/app/middlewares/error_middleware.js
@@ -1,14 +1,27 @@
 // Error handling middleware
+const ERROR_ID_MAP = {
+  ValidationError: "ERR_VALIDATION_001",
+  NotFoundError: "ERR_NOT_FOUND_002",
+  DuplicateError: "ERR_DUPLICATE_003",
+  ServiceError: "ERR_SERVICE_004",
+  AppError: "ERR_APP_005",
+};
+
+function getErrorId(err) {
+  const errorType = err.constructor.name;
+  return ERROR_ID_MAP[errorType] || "ERR_UNKNOWN_999";
+}
+
 function errorHandler(err, req, res, next) {
-  const requestId = req.id || req.headers["x-request-id"] || "unknown";
   const status = err.status || 500;
   const message = err.message || "Internal Server Error";
+  const errorId = getErrorId(err);
 
   // Attach error to response so pinoHttp recognizes it
   res.err = err;
 
   res.status(status).json({
-    id: requestId, // TODO: return error id
+    id: errorId,
     message: message,
   });
 }
